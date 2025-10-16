7062d2156ca45fd9cca50d2886483d624bbf99d6
Benjamin Tang <you@example.com>
2025-10-16 08:55:14 +0200

    Disable ESLint during builds

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: [], // Add any external image domains here if needed
   },
